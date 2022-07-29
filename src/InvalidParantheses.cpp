01e9032c8c7f77af3dfab077376dfd61cec8ebf0
ManojTGN <manoj.thunderviz@gmail.com>
2022-07-29 19:54:32 +0530

    nqueen function init

--- src/InvalidParantheses.cpp
+++ src/InvalidParantheses.cpp
@@ -2,32 +2,9 @@
 
 using namespace std;
 
-bool canPlace(){
-
-    return false;
-}
-
-void nqueen(int *board){
-    
-}
-
+//Initializing Invalid Parantheses Cpp
 
 int main(){
-    //Getting The Board Length (n)
-    int n;
-    printf("N-Queens Problem\nEnter 'n':");
-    scanf("%d",&n);
-
-    //Creating A 2D Dynamic Array (board)
-    int* board = new int[n * n];
-    for (int x = 0; x < n; x++){
-        for (int y = 0; y < n; y++) {
-            *(board + x * n + y) = 0;
-        }
-    }
-    nqueen(board);
-
-    //End
-    delete[] board;
+    printf("Invalid Parantheses Cpp");
     return 0;
 }
