38ee301c9f975236345d08c597351a8d244fde1a
ManojTGN <manoj.thunderviz@gmail.com>
2022-07-31 11:53:05 +0530

    Updated StickerToSpellWord.cpp

--- src/StickerToSpellWord.cpp
+++ src/StickerToSpellWord.cpp
@@ -1,10 +1,35 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-//Initializing Sticker To Spell Word Cpp
+
+int FindMinimumSticker(string Stickers[], string target, int stickerCount){
+    
+    string substring;string remaining;
+    if(target.length() == 0) return 1;
+
+    for(int i = target.length(); i >= 0 ; i ++){
+        for(int j = 0; j < stickerCount; j++){
+
+            substring = target.substr(0, i);
+            if(Stickers[j].find(substring)){
+                substring = target.substr(0, i);
+                remaining = target.substr(i,target.length());
+                FindMinimumSticker(Stickers,remaining,stickerCount);
+            }
+
+        }
+    }
+
+    return -1;
+}
 
 int main(){
-    printf("Sticker To Spell Word");
+    string target = "thehat";
+    string Stickers[3] = {"with","example","science"};
+
+    if(FindMinimumSticker(Stickers,target,3) == -1) cout<<"Unable To Find The Target Characters In The Stickers List"<<endl;
+
     return 0;
 }
