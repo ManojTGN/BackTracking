96e0af54d13e3f65d2af4e85fbc0b63905eed6ed
ManojTGN <manoj.thunderviz@gmail.com>
2022-08-01 10:28:29 +0530

    StickerToSpellWord.cpp Updated

--- src/StickerToSpellWord.cpp
+++ src/StickerToSpellWord.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
 #include <string>
+#include <limits>
+#define StickerCount 3
 
 using namespace std;
 
 
-int FindMinimumSticker(string Stickers[], string target, int stickerCount){
-    
-    string substring;string remaining;
-    if(target.length() == 0) return 1;
+bool FindMinimumSticker(string Stickers[], string target, int CurrentScore, int &minScore,int usedStickers[StickerCount]){
+    if(target.length() == 0){
+        if(CurrentScore < minScore) minScore = CurrentScore;
+        for(int i = 0; i < StickerCount; i++) usedStickers[i] = -1;
+        return true;
+    } 
 
-    for(int i = target.length(); i >= 0 ; i ++){
-        for(int j = 0; j < stickerCount; j++){
+    int finder;
+    for(int i = target.length(); i >= 0 ; i--){
 
-            substring = target.substr(0, i);
-            if(Stickers[j].find(substring)){
-                substring = target.substr(0, i);
-                remaining = target.substr(i,target.length());
-                FindMinimumSticker(Stickers,remaining,stickerCount);
+        string substring = target.substr(0, i);
+        for(int j = 0; j < StickerCount; j++){
+
+            finder = Stickers[j].find(substring);
+            if( finder != -1 && finder != 0 ){
+                string remaining = target.substr(i,target.length());
+                if(usedStickers[j] == 1) {
+                    FindMinimumSticker(Stickers,remaining,CurrentScore,minScore,usedStickers);
+                }else{
+                    usedStickers[j] = 1;
+                    FindMinimumSticker(Stickers,remaining,CurrentScore+1,minScore,usedStickers);
+                } 
+                
             }
 
         }
     }
 
-    return -1;
+    return false;
 }
 
 int main(){
     string target = "thehat";
-    string Stickers[3] = {"with","example","science"};
+    string Stickers[StickerCount] = {"with","example","science"};
 
-    if(FindMinimumSticker(Stickers,target,3) == -1) cout<<"Unable To Find The Target Characters In The Stickers List"<<endl;
+    int minScore = numeric_limits<int>::max();
+    int usedStickers[StickerCount] = {-1,-1,-1};
+
+    FindMinimumSticker(Stickers,target,0,minScore,usedStickers);
+    cout<<"Minimum Sticker Score: "<<minScore<<endl;
 
     return 0;
 }
